e24a117687ea0822f2b78d89c72196519076af20
arhturfary <arthurfary22@gmail.com>
2024-01-24 23:54:37 -0300

    chamada de função + slider animation

--- botaomaldito.js
+++ botaomaldito.js
@@ -2,14 +2,19 @@ let slideIndex = 0;
 let slides = document.getElementsByClassName("slide");
 
 function mover(n) {
-  slideIndex += n;
+  slideIndex += n; // slideIndex = slideIndex + n
   if (slideIndex >= slides.length) {slideIndex = 0}
   if (slideIndex < 0) {slideIndex = slides.length - 1}
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex].style.display = "block";
+
+  // for (let i = 0; i < slides.length; i++) {
+  //   slides[i].style.display = "none";
+  // }
+
+  // slides[slideIndex].style.display = "block";
+
+  document.querySelector('.slider').style.transform = `translateX(-${slideIndex * 100}%)`;
 }
+
 
 window.onload = function() {
   mover(0);
