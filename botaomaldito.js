3d77f238e7773b927a6ec2914774ee625097331e
MurilloLucindo <113224956+MurilloLucindo@users.noreply.github.com>
2024-01-26 15:03:12 -0300

    Darkmode e mais alguns modos
    o ultimo e perfeito

--- botaomaldito.js
+++ botaomaldito.js
@@ -15,6 +15,32 @@ function mover(n) {
   document.querySelector('.slider').style.transform = `translateX(-${slideIndex * 100}%)`;
 }
 
+// document.getElementById('modeSelect').addEventListener('change', function() {
+//   // Remova todas as classes de tema existentes
+//   document.documentElement.className = '';
+//   // Adicione a classe de tema selecionada
+//   document.documentElement.classList.add(this.value + '-mode');
+// });
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+  document.getElementById('themePanel').style.display = 'block';
+});
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+  document.getElementById('themePanel').classList.toggle('show');
+});
+
+document.getElementById('themePanel').addEventListener('click', function(e) {
+  if (e.target.tagName === 'A') {
+      // Remova todas as classes de tema existentes
+      document.documentElement.className = '';
+      // Adicione a classe de tema selecionada
+      document.documentElement.classList.add(e.target.dataset.theme + '-mode');
+      // Oculte o painel de temas
+      this.classList.remove('show');
+      e.preventDefault();
+  }
+});
 
 window.onload = function() {
   mover(0);
